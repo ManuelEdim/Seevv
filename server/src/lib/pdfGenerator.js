c90a91e1ccc4a3576ee4e7d6f00d43a64649a9b5
Hellodreamivae <80988406+ManuelEdim@users.noreply.github.com>
2026-04-30 18:55:35 +0100

    Fix Render deployment: Node version, Puppeteer flags, render.yaml

--- server/src/lib/pdfGenerator.js
+++ server/src/lib/pdfGenerator.js
@@ -28,8 +28,13 @@ export const generateCVPdf = async (cvData) => {
   });
 
   const browser = await puppeteer.launch({
-    headless: "new",
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+    ],
   });
 
   try {
